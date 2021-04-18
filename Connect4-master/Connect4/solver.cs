689c54434c77a2eec1e6785a81c692d3af1d1067
asanc <sanchezorosco_alexis@hotmail.com>
2021-04-17 20:59:53 -0500

    Agregue comentarios

--- Connect4-master/Connect4/solver.cs
+++ Connect4-master/Connect4/solver.cs
@@ -6,30 +6,30 @@ namespace con4
 {
     class solver
     {
-        long[] bitboard;
-        int counter = 0;
-        int[] height;
-        int [] moves;
-        public solver()
+        long[] bitboard;    //Tablero
+        int counter = 0;    //Contador para saber quien es el tirador
+        int[] height;       //Indica siguiente posición donde puedes poner fichar
+        int [] moves;       //Hace movimiento y los guarda
+        public solver() //Constructor del solver
         {
             bitboard = new long[2];
             height = new int[10];
             moves = new int[500];
         }
 
-        void makeMove(int col)
+        void makeMove(int col) //Hacen movimiento
         {            
             long move = 1 << height[col]++; // (1)            
             bitboard[counter & 1] ^= move;  // (2)
             moves[counter++] = col;         // (3)
         }
-        void undoMove()
+        void undoMove() //Deshacen movimiento
         {
             int col = moves[--counter];     // reverses (3)
             long move = 1L << --height[col]; // reverses (1)
             bitboard[counter & 1] ^= move;  // reverses (2)
         }
-        List<int> listMoves()
+        List<int> listMoves()   //Lista de de movimientos validos
         {
             List<int> moves = new List<int>();
             long TOP = 0b1000000_1000000_1000000_1000000_1000000_1000000_1000000L;
@@ -39,12 +39,11 @@ namespace con4
             }
             return moves;
         }
-        bool isWin(long bitboard)
+        bool isWin(long bitboard)   //Indica si la posición actual es ganadora
         {            
-
             return false;
         }
-        int evalPosition(long pos)
+        int evalPosition(long pos)  //
         {
             List<int> nextMov = listMoves();
             int winningMovs = 0;
