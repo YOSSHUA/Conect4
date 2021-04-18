38cec3ebd23f061f64569cbae238e0094dc94310
asanc <sanchezorosco_alexis@hotmail.com>
2021-04-17 21:39:03 -0500

    Agregamos minimax

--- Connect4-master/Connect4/solver.cs
+++ Connect4-master/Connect4/solver.cs
@@ -16,36 +16,47 @@ namespace con4
             height = new int[10];
             moves = new int[500];
         }
-
-        void makeMove(int col) //Hacen movimiento
-        {            
-            long move = 1 << height[col]++; // (1)            
-            bitboard[counter & 1] ^= move;  // (2)
-            moves[counter++] = col;         // (3)
-        }
-        void undoMove() //Deshacen movimiento
+        private void makeMove(int col) //Hacen movimiento
         {
-            int col = moves[--counter];     // reverses (3)
-            long move = 1L << --height[col]; // reverses (1)
-            bitboard[counter & 1] ^= move;  // reverses (2)
+            long move = 1L << height[col]++;
+            bitboard[counter & 1] ^= move;
+            moves[counter++] = col;
         }
-        List<int> listMoves()   //Lista de de movimientos validos
+        private void undoMove()//Deshacen movimiento
+        {
+            int col = moves[--counter];
+            long move = 1L << --height[col];
+            bitboard[counter & 1] ^= move;
+        }
+
+        private Boolean isWin(long bitboard)//Indica si la posición actual es ganadora
+        {
+            int[] directions = { 1, 7, 6, 8 };
+            for (int i = 0; i < directions.Length; i++)
+            {
+                if ((bitboard & (bitboard >> directions[i]) &
+                   (bitboard >> (2 * directions[i])) & (bitboard >> (3 * directions[i]))) != 0)
+                    return true;
+
+            }
+            return false;
+        }
+
+        List<int> validMoves()//Lista de de movimientos validos
         {
             List<int> moves = new List<int>();
             long TOP = 0b1000000_1000000_1000000_1000000_1000000_1000000_1000000L;
             for (int col = 0; col <= 6; col++)
             {
-                if ((TOP & (1L << height[col])) == 0) moves.Add(col);
+                if ((TOP & (1L << height[col])) == 0)
+                    moves.Add(col);
+
             }
             return moves;
         }
-        bool isWin(long bitboard)   //Indica si la posición actual es ganadora
-        {            
-            return false;
-        }
         int evalPosition(long pos)  //
         {
-            List<int> nextMov = listMoves();
+            List<int> nextMov = validMoves();
             int winningMovs = 0;
             foreach(int col in nextMov)
             {
@@ -58,6 +69,62 @@ namespace con4
             }
             return winningMovs;
 
+        }
+        int maxVal = 500;
+        int bestMov;
+        int maxDepth=100;
+        int minimax(int alpha, int beta, int depth)
+        {
+            if(isWin(bitboard[counter & 1]))
+            {
+                if ((counter & 1) == 1)
+                    return -maxVal;
+                else
+                    return maxVal;
+            }
+            if (depth == 0)
+            {
+                if ((counter & 1) == 1)
+                    return evalPosition(bitboard[counter & 1]);
+                else
+                    return evalPosition(bitboard[counter & 1]);
+            }
+            if ((counter & 1) == 0)
+            {
+                List<int> listMov = validMoves();
+                int bestValue = -maxVal;
+                int len = listMov.Count;
+                for (int i = 0; i < len; i++)
+                {
+                    makeMove(listMov[i]);
+                    int value = minimax(alpha, beta, depth - 1);
+                    undoMove();
+                    bestValue = Math.Max(bestValue, value);
+                    if (maxDepth == depth && bestValue == value)
+                        bestMov = listMov[i];
+                    alpha = Math.Max(alpha, bestValue);
+                    if (alpha >= beta)
+                        break;
+                }
+                return bestValue;
+            }
+            else
+            {
+                List<int> listMov = validMoves();
+                int bestValue = maxVal;
+                int len = listMov.Count;
+                for (int i = 0; i < len; i++)
+                {
+                    makeMove(listMov[i]);
+                    int value = minimax(alpha, beta, depth - 1);
+                    undoMove();
+                    bestValue = Math.Min(bestValue, value);
+                    alpha = Math.Min(alpha, bestValue);
+                    if (alpha >= beta)
+                        break;
+                }
+                return bestValue;
+            }
         }
     }
 
